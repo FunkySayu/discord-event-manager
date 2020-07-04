d13d72b245819ed7fcb3ea895f56d141dbeccfb5
Axel FunkySayu Martin <funkysayu@gmail.com>
2020-07-04 22:11:26 +0200

    Provide a way to update entries in the spreadsheet.
    I went for the easy solution of deleting everything related to a player and then updating it.
    I think it'll be good avoiding that later, as we'll likely want to annotate players with stuff
    directly in the spreadsheet (e.g. comments, officer management shit etc).

--- roster/sheet_integration.py
+++ roster/sheet_integration.py
@@ -19,7 +19,10 @@ limitations under the License.
 """
 
 import asyncio
+import logging
+import threading
 
+from tabulate import tabulate
 from typing import List, Dict, Any
 
 from google_integration.handler import get_handler
@@ -31,26 +34,55 @@ class RosterSpreadsheet:
 
     :attr _SPREADSHEET_ID: The Google spreadsheet ID to retrieve information from.
     :attr _ROSTER_SHEET: The Spreadsheet tab to edit / pull data from.
+    :attr _LINE_START_OFFSET: Beginning of the player list (i.e. without the headers).
     """
 
-    _SPREADSHEET_ID: string
-    _ROSTER_SHEET: string
+    LINE_START_OFFSET = 3
 
-    def __init__(self, handler, spreadsheet_id, roster_sheet):
+    _SPREADSHEET_ID: string
+    _ROSTER_SHEET_A1_SELECTOR: string
+
+    def __init__(self, handler, spreadsheet_id, roster_sheet_name):
         self._SPREADSHEET_ID = spreadsheet_id
-        self._ROSTER_SHEET = roster_sheet
+        self._ROSTER_SHEET_A1_SELECTOR = f'{roster_sheet_name}!A{self.LINE_START_OFFSET}:F1000'
         self._handler = handler
+        self._mutex = threading.Lock()
+
+        # Google Spreadsheets API have two different notations to access a specific
+        # sheet in a spreadsheet: using the A1 notation (for range of values selection),
+        # the name of the spreadsheet needs to be used, while some other endpoints
+        # may require the sheet ID (int32).
+        # So we need to resolve what's the sheet ID associated to the sheet name
+        # provided as argument. That's quite stupid but I'm getting used to that.
+        spreadsheet_metadata = self._handler.get(spreadsheetId=self._SPREADSHEET_ID).execute()
+        self._ROSTER_SHEET_ID = None
+        for sheet_metadata in spreadsheet_metadata.get('sheets', []):
+            if 'properties' not in sheet_metadata:
+                continue
+            properties = sheet_metadata['properties']
+            if properties.get('title') != roster_sheet_name:
+                continue
+            self._ROSTER_SHEET_ID = properties.get('sheetId')
+        if self._ROSTER_SHEET_ID is None:
+            raise KeyError('Sheet name {} is missing in the spreadsheet {}'.format(
+                roster_sheet_name, self._SPREADSHEET_ID))
 
     async def get_players(self) -> List[Player]:
         """Retrieves the list of players as configured in the spreadsheet."""
-        cursor = self._handler.values().get(spreadsheetId=self._SPREADSHEET_ID,
-                                            range=f"{self._ROSTER_SHEET}!A3:F1000")
-        data = await self._execute(cursor)
+        cursor = self._handler.values().get(
+            spreadsheetId=self._SPREADSHEET_ID,
+            range=self._ROSTER_SHEET_A1_SELECTOR)
+
+        with self._mutex:
+            data = await self._execute(cursor)
         assert data.get('values') is not None
-        assert all(len(columns) == 5 for columns in data['values'])
 
         player_by_uid = {}
-        for [handle, uid, server, name, klass_name] in data['values']:
+        for row in data['values']:
+            if len(row) < 5:
+                # Incomplete inputs.
+                continue
+            [handle, uid, server, name, klass_name] = row
             if not (handle and uid and server and name and klass_name):
                 # Incomplete inputs.
                 continue
@@ -59,6 +91,61 @@ class RosterSpreadsheet:
             player = player_by_uid[uid]
             player.characters.append(Character(server, name, CharacterClass(klass_name)))
         return list(player_by_uid.values())
+
+    async def update_player(self, player: Player):
+        """Updates the list of characters for the given player."""
+        with self._mutex:
+            cursor = self._handler.values().get(
+                spreadsheetId=self._SPREADSHEET_ID,
+                range=self._ROSTER_SHEET_A1_SELECTOR)
+            data = await self._execute(cursor)
+
+            # Iterate in reverse order, so we can remove the rows without
+            # having to deal with offsetting more and more the indexes.
+            delete_requests = []
+            for index, row in reversed(list(enumerate(data['values']))):
+                if len(row) >= 2 and row[1] == player.discord_uuid:
+                    # Delete any rows matching the user.
+                    logging.debug(
+                        'Deleting row %d (matching player %s)',
+                        self.LINE_START_OFFSET+index,
+                        player.discord_handle)
+                    delete_requests.append({"deleteDimension": {"range": {
+                        "sheetId": self._ROSTER_SHEET_ID,
+                        "dimension": "ROWS",
+                        "startIndex": self.LINE_START_OFFSET+ index-1,
+                        "endIndex": self.LINE_START_OFFSET+index,
+                    }}})
+
+            if delete_requests:
+                cursor = self._handler.batchUpdate(
+                    spreadsheetId=self._SPREADSHEET_ID,
+                    body={'requests': delete_requests})
+                await self._execute(cursor)
+
+            # Now re-introduce the characters of the players, if any.
+            if len(player.characters) < 1:
+                return
+
+            new_rows = []
+            for character in player.characters:
+                new_rows.append([
+                    player.discord_handle,
+                    player.discord_uuid,
+                    character.server,
+                    character.name,
+                    character.klass.value,
+                ])
+            logging.debug(
+                'Inserting the following %d rows: \n%s',
+                len(new_rows),
+                tabulate(new_rows))
+            cursor = self._handler.values().append(
+                spreadsheetId=self._SPREADSHEET_ID,
+                range=self._ROSTER_SHEET_A1_SELECTOR,
+                valueInputOption="USER_ENTERED",
+                body={'values': new_rows})
+            await self._execute(cursor)
 
     async def _execute(self, cursor) -> Dict[string, Any]:
         """Wraps cursor execution in an asyncio call."""
