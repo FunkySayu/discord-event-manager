151dff2480d91b53ccf3df6e38f88c1cff7ea34f
funkysayu <funkysayu@gmail.com>
2020-08-02 17:16:33 +0200

    Add a header component displaying a toolbar with profile controls.

---
+++ ui/web/src/app/header/header.component.spec.ts
@@ -0,0 +1,79 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { async, fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { UserProfile } from '../user/user.service';
+
+import { HeaderModule } from './header.module';
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule, RouterTestingModule, HeaderModule ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('provides a link to login to discord if no user is retrieved', () => {
+    const loginButton = fixture.nativeElement.querySelector('a.login');
+    expect(loginButton).not.toBeNull();
+    expect(loginButton.getAttribute('href')).toBe('/auth/discord/oauth');
+  });
+
+  it('displays the user avatar instead of the login button after auth', () => {
+    const profile: UserProfile = {
+      guilds: [],
+      user: {id: '123', avatar: '456'},
+    };
+    component.profile = profile;
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('a.login')).toBeNull();
+    const userButton = fixture.nativeElement.querySelector('button.user-profile');
+    expect(userButton).not.toBeNull();
+    expect(userButton.querySelector('discord-icon img')).not.toBeNull();
+  });
+
+  it('selects a guild by default if none are selected', fakeAsync(() => {
+    const profile: UserProfile = {
+      guilds: [{id: '123', name: 'My amazing guild', icon: '456'}],
+    };
+    component.profile = profile;
+    component.ngOnChanges();
+    fixture.detectChanges();
+    // Guild default selection happens on the next cycle; wait for it.
+    tick(1);
+
+    const guildSelector = fixture.nativeElement.querySelector('button.guild-selection')
+    expect(guildSelector).not.toBeNull();
+    expect(guildSelector.querySelector('discord-icon img')).not.toBeNull();
+    expect(guildSelector.textContent).toContain('My amazing guild');
+  }));
+});
---
+++ ui/web/src/app/header/header.component.ts
@@ -0,0 +1,63 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService, UserProfile, Guild } from 'src/app/user/user.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnChanges {
+  @Input() profile?: UserProfile;
+  @Input() selectedGuild?: Guild;
+  @Output() selectedGuildChange = new EventEmitter<Guild>();
+
+  constructor(private readonly router: Router, private readonly userService: UserService) { }
+
+  ngOnChanges() {
+    if (this.profile && this.profile.guilds.length && !this.selectedGuild) {
+      this.selectedGuild = this.profile.guilds[0];
+      // Emit the automatic selection on the next cycle, to avoid creating an
+      // Angular error where states are mis-matching.
+
+      setTimeout(() => {
+        this.selectedGuildChange.emit(this.selectedGuild);
+      });
+    }
+  }
+
+  onGuildSelected(guild: Guild) {
+    if (this.selectedGuild?.id !== guild.id) {
+      this.selectedGuild = guild;
+      this.selectedGuildChange.emit(guild);
+    }
+  }
+
+  logoutUser() {
+    this.userService.logout().subscribe(() => {
+      this.router.navigate(['/'], {queryParams: {'refresh': 1}});
+    });
+  }
+
+  trackByGuildId(index: number, guild: any): string {
+    return guild.id;
+  }
+}
