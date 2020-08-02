41fbda6476842bc4a9dd1a87eb61e8be7dc0f990
funkysayu <funkysayu@gmail.com>
2020-08-02 16:53:09 +0200

    Add a service interacting with the user & auth endpoint

---
+++ ui/web/src/app/user/user.service.ts
@@ -0,0 +1,62 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, mapTo } from 'rxjs/operators';
+
+/** A Discord guild information. */
+export interface Guild {
+  id?: string;
+  name?: string;
+}
+
+/** High level information about an user. */
+export interface UserProfile {
+  guilds?: Guild[];
+}
+
+/** Response to the authentication check. */
+interface AuthCheckResponse {
+  authenticated?: boolean;
+}
+
+
+/** Accesses the general profile of the user. */
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private readonly http: HttpClient) { }
+
+  /** Returns the high level user profile. */
+  getUserProfile(): Observable<UserProfile> {
+    return this.http.get<UserProfile>('/api/user');
+  }
+
+  /** Checks if the user is currently authenticated. */
+  isAuthenticated(): Observable<boolean> {
+    return this.http.get<AuthCheckResponse>('/auth/discord/is_authenticated').pipe(
+      map(response => !!response?.authenticated));
+  }
+
+  /** Logs out the user. */
+  logout(): Observable<void> {
+    return this.http.get('/auth/discord/logout').pipe(mapTo(undefined));
+  }
+}
