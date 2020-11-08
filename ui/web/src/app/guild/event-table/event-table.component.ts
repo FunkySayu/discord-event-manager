ed41b93194bc1fb2d84f1f3b0ab440e35a634a19
Axel FunkySayu Martin <funkysayu@gmail.com>
2020-11-08 22:41:47 +0100

    web/roster-member Change event-table's layout from flex to table.

--- ui/web/src/app/guild/event-table/event-table.component.ts
+++ ui/web/src/app/guild/event-table/event-table.component.ts
@@ -72,9 +72,8 @@ class GuildEventTableDataSource extends DataSource<Event | ExpandedEvent> {
   styleUrls: ['./event-table.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', visibility: 'hidden'})),
+      state('collapsed', style({height: '0px', minHeight: '0', visibility: 'hidden', display: 'none'})),
       state('expanded', style({height: '*', visibility: 'visible'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
 })
