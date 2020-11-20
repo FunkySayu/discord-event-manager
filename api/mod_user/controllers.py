c6c5d5242b4368351acd4db8dfb570fcc9762db7
Axel FunkySayu Martin <funkysayu@gmail.com>
2020-11-21 00:35:31 +0100

    api/character-user-link add a route associating a user with a character.

--- api/mod_user/controllers.py
+++ api/mod_user/controllers.py
@@ -15,10 +15,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
+from api.base import db
+from config.blizzard import get_wow_handler
 from api.mod_auth.session import get_discord_session
-from api.mod_user.user import User
+from api.mod_user.user import User, UserOwnsCharacters
+from api.mod_user.forms import CharacterAssociationForm
 
 mod_user = Blueprint('user', __name__, url_prefix='/api/user')
 
@@ -29,3 +32,24 @@ def user():
     discord_session = get_discord_session()
     user = User.from_oauth_discord(discord_session)
     return jsonify(user.to_dict())
+
+
+@mod_user.route('/<user_id>/characters', methods=['PUT', 'POST'])
+def add_character(user_id: str):
+    """Associates a character to the logged in user."""
+    # TODO(funkysayu): Implement the visibility restriction.
+    form = CharacterAssociationForm.from_json(request.get_json())
+    if not form.validate():
+        return jsonify(error='Invalid request', form_errors=form.errors), 400
+
+    character = form.get_character(get_wow_handler())
+    user = User.query.filter_by(id=user_id).one_or_none()
+    if user is None:
+        return jsonify(error='User not found'), 404
+
+    relationship = UserOwnsCharacters(user.id, character.id)
+    db.session.add(character)
+    db.session.add(relationship)
+    db.session.commit()
+
+    return jsonify(character.to_dict())
