1305e635b7f8db03534735fb58c8fc78ce382692
Axel FunkySayu Martin <funkysayu@gmail.com>
2020-11-22 00:01:38 +0100

    web/add-character Better reflect the backend model in the FE wow API

--- web/src/app/wow/wow.service.ts
+++ web/src/app/wow/wow.service.ts
@@ -17,51 +17,69 @@
 
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
-/** All the possible classes for a World of Warcraft character. */
-export enum WowCharacterClass {
-  DEATH_KNIGHT = 'DEATH_KNIGHT',
-  DEMON_HUNTER = 'DEMON_HUNTER',
-  DRUID = 'DRUID',
-  HUNTER = 'HUNTER',
-  MAGE = 'MAGE',
-  MONK = 'MONK',
-  PALADIN = 'PALADIN',
-  PRIEST = 'PRIEST',
-  ROGUE = 'ROGUE',
-  SHAMAN = 'SHAMAN',
-  WARLOCK = 'WARLOCK',
-  WARRIOR = 'WARRIOR',
+
+/** Represents a World of Warcraft region. */
+export type Region = string;
+
+
+/** The two available factions in WoW. */
+export enum WowFaction {
+  HORDE = 'HORDE',
+  ALLIANCE = 'ALLIANCE',
 }
 
-/** The 3 possible roles on World of Warcraft. */
-export enum WowRole {
-  TANK = 'TANK',
-  DPS = 'DPS',
-  HEALER = 'HEALER',
+
+/** A world of warcraft realm. */
+export declare interface WowRealm {
+  name?: string;
+  slug?: string;
+  region?: Region;
+  timezone_name?: string;
 }
+
+
+/** A world of warcraft playable speciality of a class. */
+export declare interface WowPlayableSpec {
+  name?: string;
+  role?: string;
+  klass?: WowPlayableClass;
+}
+
+
+/** A world of warcraft playable class. */
+export declare interface WowPlayableClass {
+  name?: string;
+  specs?: WowPlayableSpec[];
+}
+
 
 /** A world of warcraft character. */
 export declare interface WowCharacter {
   name?: string;
-  realm?: string;
-  class?: WowCharacterClass;
-  role?: WowRole;
-  speciality?: string;
-  ilvl?: number;
+  realm?: WowRealm;
+  faction?: WowFaction;
+  klass?: WowPlayableClass;
+  active_spec?: WowPlayableSpec;
+  average_ilvl?: number;
+  equipped_ilvl?: number;
   icon_url?: string;
 }
+
 
 /** A sample character for testing. */
 export const SAMPLE_CHARACTER: WowCharacter = {
   name: 'Funkypewpew',
-  realm: 'Argent Dawn',
-  class: WowCharacterClass.HUNTER,
-  role: WowRole.DPS,
-  speciality: 'Marksmanship',
-  ilvl: 430,
+  realm: {name: 'Argent Dawn', slug: 'argent-dawn'},
+  klass: {name: 'Hunter', specs: []},
+  //role: WowRole.DPS,
+  //speciality: 'Marksmanship',
+  average_ilvl: 430,
   icon_url: 'http://render-eu.worldofwarcraft.com/character/argent-dawn/100/146666340-avatar.jpg',
 };
+
 
 /** Provides access to the WoW related routes. */
 @Injectable({
@@ -69,4 +87,21 @@ export const SAMPLE_CHARACTER: WowCharacter = {
 })
 export class WowService {
   constructor(private readonly http: HttpClient) {}
+
+  /** Get all regions supported by the application. */
+  getRegions(): Observable<Region[]> {
+    return this.http.get<{regions: Region[]}>('/api/wow/region/').pipe(
+      map(response => response?.regions ?? []));
+  }
+
+  /** Get the world of warcraft realms associated with a region. */
+  getRealms(region: Region): Observable<WowRealm[]> {
+    return this.http.get<{realms: WowRealm[]}>(`/api/wow/region/${region}/realm/`).pipe(
+      map(response => response?.realms ?? []));
+  }
+
+  /** Gets a character from the world of warcraft api. */
+  getCharacter(region: Region, realmSlug: string, character: string): Observable<WowCharacter> {
+    return this.http.get(`/api/wow/region/${region}/realm/${realmSlug}/character/${character}`);
+  }
 }
