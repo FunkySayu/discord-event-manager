6cf2fa6eaec3a945c2603e11f1c5c9db86da8674
Axel FunkySayu Martin <funkysayu@gmail.com>
2020-11-21 13:06:44 +0100

    api/get-or-create Create methods querying from database instead of WowAPI for character data

--- api/mod_wow/character_test.py
+++ api/mod_wow/character_test.py
@@ -20,6 +20,7 @@ import json
 import os
 import unittest.mock
 
+from api.common.testing import DatabaseTestFixture
 from api.mod_wow.character import WowCharacter
 from api.mod_wow.region import Region
 from api.mod_wow.realm import WowRealm
@@ -31,7 +32,7 @@ TESTDATA_DIR = os.path.join(
     'testdata')
 
 
-class TestWowPlayableSpecModel(unittest.TestCase):
+class TestWowPlayableSpecModel(DatabaseTestFixture, unittest.TestCase):
     """Checks creation of a wow playable spec."""
 
     @classmethod
@@ -46,11 +47,13 @@ class TestWowPlayableSpecModel(unittest.TestCase):
         mock = unittest.mock.MagicMock()
         mock.get_character_profile_summary.return_value = self.CHARACTER_DATA
 
-        realm = WowRealm(536, 'Argent Dawn', 'argent-dawn', Region.eu, 'Europe/Paris')
+        realm = WowRealm(id=536, region=Region.eu, name='Argent Dawn',
+                         slug='argent-dawn', timezone_name='Europe/Paris')
         character = WowCharacter.create_from_api(mock, realm, 'Funkypewpew')
 
-        mock.get_character_profile_summary.assert_called_with('eu', 'profile-eu', 'argent-dawn', 'Funkypewpew', locale='en_US')
-        self.assertEqual(character.id, 146666340)
+        mock.get_character_profile_summary.assert_called_with(
+            'eu', 'profile-eu', 'argent-dawn', 'Funkypewpew', locale='en_US')
+        self.assertEqual(character.id, '146666340')
         self.assertEqual(character.name, 'Funkypewpew')
         self.assertEqual(character.realm_id, 536)
         self.assertEqual(character.faction, WowFaction.alliance)
@@ -58,3 +61,30 @@ class TestWowPlayableSpecModel(unittest.TestCase):
         self.assertEqual(character.active_spec_id, 253)
         self.assertEqual(character.average_ilvl, 136)
         self.assertEqual(character.equipped_ilvl, 135)
+
+    def test_get_or_create_queries_api(self):
+        """Tests the character is queried if not available in database."""
+        mock = unittest.mock.MagicMock()
+        mock.get_character_profile_summary.return_value = self.CHARACTER_DATA
+
+        realm = WowRealm(id=536, region=Region.eu, slug='argent-dawn')
+        character = WowCharacter.get_or_create(mock, realm, 'Funkypewpew')
+
+        mock.get_character_profile_summary.assert_called_with(
+            'eu', 'profile-eu', 'argent-dawn', 'Funkypewpew', locale='en_US')
+        self.assertEqual(character.id, '146666340')
+        self.assertEqual(character.name, 'Funkypewpew')
+
+    def test_get_or_create_uses_cache(self):
+        """Tests the character is queried from database if available there."""
+        mock = unittest.mock.MagicMock()
+        mock.get_character_profile_summary.return_value = self.CHARACTER_DATA
+        realm = WowRealm(id=536, region=Region.eu, slug='argent-dawn')
+        stored_character = WowCharacter(id='123456', name='Funkypewpew', realm_id=realm.id, realm=realm)
+        self.db.session.add(stored_character)
+
+        character = WowCharacter.get_or_create(mock, realm, 'Funkypewpew')
+
+        mock.get_character_profile_summary.assert_not_called()
+        self.assertEqual(character.id, '123456')
+        self.assertEqual(character.name, 'Funkypewpew')
